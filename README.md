1b21435b32ca31cb8087b3ae7d8dac907b86adb8
vittfiles <vittfiles@gmail.com>
2024-08-11 13:11:17 -0300

    webpage created

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center"> Alura & Oracle Next Education Challenge: Code encrypter </h1>
+
+<div align="center">
+   This is a challenge for <a href="https://www.aluracursos.com/" target="_blank">Alura Latam</a> and <a href="https://www.oracle.com/ar/education/oracle-next-education/" target="_blank">Oracle Next Education</a>.
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Developers](#Developers)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+The webpage encrypt and decrypt a text.
+The encryptation code works by changing the vowels:
+"e" to "enter"
+"i" to "imes"
+"a" to "ai"
+"o" to "ober"
+"u" to "ufat"
+
+## Developers
+
+- <a href="https://www.linkedin.com/in/matias-m-79b5652a0/" target="_blank">Matias M. (vittfiles).</a>
---
+++ script.js
@@ -0,0 +1,74 @@
+function isUppercase(text) {
+  let regex = new RegExp("[A-Z]");
+  if (regex.test(text)) {
+    return true;
+  }
+  return false;
+}
+
+function encrypt(text) {
+  let regexp = new RegExp("a|e|i|o|u");
+  let res = "";
+  let rest = text;
+  let data = null;
+  while ((data = regexp.exec(rest))) {
+    res += rest.substring(0, data.index);
+    switch (data[0]) {
+      case "a":
+        res += "ai";
+        break;
+      case "e":
+        res += "enter";
+        break;
+      case "i":
+        res += "imes";
+        break;
+      case "o":
+        res += "ober";
+        break;
+      case "u":
+        res += "ufat";
+        break;
+    }
+    rest = rest.substring(data.index + 1, rest.length);
+  }
+  return res + rest;
+}
+
+function decrypt(text) {
+  let res = text;
+  res = res.replaceAll("ai", "a");
+  res = res.replaceAll("enter", "e");
+  res = res.replaceAll("imes", "i");
+  res = res.replaceAll("ober", "o");
+  res = res.replaceAll("ufat", "u");
+  return res;
+}
+
+function init() {
+  let textElement = document.getElementById("text");
+  let resultElement = document.getElementById("result");
+
+  let divResultElement = document.getElementById("div-result");
+  let infoElement = document.getElementById("info");
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#encrypt")) {
+      if (!isUppercase(textElement.value)) {
+        resultElement.innerText = encrypt(textElement.value);
+        textElement.value = "";
+        divResultElement.classList.remove("hidden");
+        infoElement.classList.add("hidden");
+      }
+    } else if (e.target.matches("#decrypt")) {
+      if (!isUppercase(textElement.value)) {
+        resultElement.innerText = decrypt(textElement.value);
+        textElement.value = "";
+      }
+    } else if (e.target.matches("#copy_text")) {
+      navigator.clipboard.writeText(resultElement.innerText);
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", init);
